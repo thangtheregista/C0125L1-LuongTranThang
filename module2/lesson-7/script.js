67036ef1df169411d4f473cd0c03a2d6ec1d06a5
thangtheregista <thangtheregista@gmail.com>
2025-03-19 15:21:56 +0700

    lesson-7-bai-tap-1

---
+++ module2/lesson-7/script.js
@@ -0,0 +1,24 @@
+let a = document.getElementById('variable1').value;
+let b = +document.getElementById('variable2').value;
+
+function add() {
+    console.log(a);
+}
+
+function subtract() {
+    let a = +document.getElementById('variable1').value;
+    let b = +document.getElementById('variable2').value;
+    console.log(a - b);
+}
+
+function multiply() {
+    let a = +document.getElementById('variable1').value;
+    let b = +document.getElementById('variable2').value;
+    console.log(a * b);
+}
+
+function divide() {
+    let a = +document.getElementById('variable1').value;
+    let b = +document.getElementById('variable2').value;
+    console.log(a / b);
+}
